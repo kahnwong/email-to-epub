0517a52f19fea853d1eaf2e82c0a493d0aab0300
kahnwong <karn@karnwong.me>
2023-09-30 16:11:48 +0700

    move error handling to to function

--- main.go
+++ main.go
@@ -10,20 +10,20 @@ import (
 	// "github.com/emersion/go-imap"
 )
 
-func loginToIMAPServer() (*client.Client, error) {
+func loginToIMAPServer() *client.Client {
 	imapAddress := fmt.Sprintf("%s:%s", os.Getenv("IMAP_SERVER_HOST"), os.Getenv("IMAP_SERVER_PORT"))
 	c, err := client.DialTLS(imapAddress, nil)
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 	log.Println("Connected")
 
 	if err := c.Login(os.Getenv("IMAP_USERNAME"), os.Getenv("IMAP_PASSWORD")); err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 	log.Println("Logged in")
 
-	return c, nil
+	return c
 }
 
 func isFolderEpubEmpty(c *client.Client) bool {
@@ -31,11 +31,8 @@ func isFolderEpubEmpty(c *client.Client) bool {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Flags for INBOX:", mbox.Flags)
 
 	// Check if empty
-	fmt.Println(mbox.Messages)
-
 	if mbox.Messages == 0 {
 		log.Println("No emails in epub folder")
 		return true
@@ -52,11 +49,9 @@ func main() {
 	}
 
 	// login to imap server
-	c, err := loginToIMAPServer()
-	if err != nil {
-		log.Fatal(err)
-	}
+	c := loginToIMAPServer()
 
+	// check if folder is empty
 	isEmptyEmail := isFolderEpubEmpty(c)
 	fmt.Println(isEmptyEmail)
 }
