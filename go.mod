5c23097ed1733dc14334af023e755e99d8297da8
kahnwong <karn@karnwong.me>
2023-09-30 20:00:28 +0700

    write emails to eml

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.20
 
 require (
 	github.com/emersion/go-imap v1.2.1
+	github.com/emersion/go-message v0.17.0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"regexp"
+	"strings"
+
 	"os"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-message/mail"
 	"github.com/joho/godotenv"
 )
 
@@ -56,7 +61,7 @@ func getUnreadMessages(c *client.Client, mailbox string, n uint32) *imap.SeqSet 
 	seqset := new(imap.SeqSet)
 	seqset.AddRange(from, to)
 
-	messages := make(chan *imap.Message, 10)
+	messages := make(chan *imap.Message, n)
 	done := make(chan error, 1)
 	go func() {
 		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
@@ -81,20 +86,139 @@ func moveMessagesToDestination(c *client.Client, seqset *imap.SeqSet) {
 	}
 }
 
+func sanitizeFilename(filename string) string {
+	// Remove non-alphanumeric characters, spaces, and replace with underscores
+	re := regexp.MustCompile(`[^\w\s-]`)
+	filename = re.ReplaceAllString(filename, "_")
+
+	// Replace multiple spaces with a single underscore
+	filename = regexp.MustCompile(`\s+`).ReplaceAllString(filename, "_")
+
+	// Trim leading and trailing spaces and underscores
+	filename = strings.Trim(filename, "_")
+
+	// Truncate to a reasonable length (e.g., 255 characters)
+	if len(filename) > 255 {
+		filename = filename[:255]
+	}
+
+	return filename
+}
+
 func main() {
+	// init
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	outputPath := "emails"
+	if _, err := os.Stat(outputPath); os.IsNotExist(err) {
+		err := os.Mkdir(outputPath, os.ModeDir|0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// app
 	c := loginToIMAPServer()
 
 	isEmptyEmail := isFolderEpubEmpty(c)
 
+	n := uint32(10) // debug
+
 	if isEmptyEmail {
-		seqset := getUnreadMessages(c, os.Getenv("SOURCE_MAILBOX"), 10)
+		seqset := getUnreadMessages(c, os.Getenv("SOURCE_MAILBOX"), n)
 		moveMessagesToDestination(c, seqset)
 
-		log.Println("Done")
+		// write messages to file
+		seqsetToFile := getUnreadMessages(c, os.Getenv("DESTINATION_MAILBOX"), n)
+
+		var section imap.BodySectionName
+		items := []imap.FetchItem{section.FetchItem()}
+
+		messages := make(chan *imap.Message, n)
+		go func() {
+			if err := c.Fetch(seqsetToFile, items, messages); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		for msg := range messages {
+			fmt.Println(msg.Body)
+
+			////////////////////////////////////////////////////////////////
+			r := msg.GetBody(&section)
+
+			mr, err := mail.CreateReader(r)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			body := ""
+
+			header := mr.Header
+			subject, err := header.Subject()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					log.Fatal(err)
+				}
+
+				switch p.Header.(type) {
+				case *mail.InlineHeader:
+					b, _ := io.ReadAll(p.Body)
+					body = string(b)
+				}
+			}
+
+			fmt.Println(body)
+			////////////////////////////////////////////////////////////////
+
+			// Create a new file
+			file, err := os.Create(fmt.Sprintf("%s/%s.eml", outputPath, sanitizeFilename(subject)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+
+			var h mail.Header
+			h.SetSubject(subject)
+
+			mw, err := mail.CreateWriter(file, h)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Create a text part
+			tw, err := mw.CreateInline()
+			if err != nil {
+				log.Fatal(err)
+			}
+			var th mail.InlineHeader
+			th.Set("Content-Type", "text/html")
+			w, err := tw.CreatePart(th)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			_, err = io.WriteString(w, body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			w.Close()
+			tw.Close()
+
+			mw.Close()
+
+			log.Println("Done")
+		}
 	}
 }
