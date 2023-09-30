8902ef2fec29d6f576b44b612be8c416e7f55e37
kahnwong <karn@karnwong.me>
2023-09-30 16:51:43 +0700

    refactor: add getUnreadMessages

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 	"github.com/joho/godotenv"
 	// "github.com/emersion/go-imap"
@@ -27,7 +28,7 @@ func loginToIMAPServer() *client.Client {
 }
 
 func isFolderEpubEmpty(c *client.Client) bool {
-	mbox, err := c.Select("epub", false)
+	mbox, err := c.Select(os.Getenv("DESTINATION_MAILBOX"), false)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,7 +39,43 @@ func isFolderEpubEmpty(c *client.Client) bool {
 		return true
 	}
 
+	log.Println("EPUB folder not empty")
 	return false
+}
+
+func getUnreadMessages(c *client.Client, n uint32) *imap.SeqSet {
+	// Select INBOX
+	mbox, err := c.Select(os.Getenv("SOURCE_MAILBOX"), false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get last n messages
+	from := uint32(1)
+	to := mbox.Messages
+	if mbox.Messages > (n - 1) {
+		// We're using unsigned integers here, only subtract if the result is > 0
+		from = mbox.Messages - (n - 1)
+	}
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(from, to)
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+
+	log.Printf("Last %d messages", n)
+	for msg := range messages {
+		log.Println("* " + msg.Envelope.Subject)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	return seqset
 }
 
 func main() {
@@ -53,5 +90,17 @@ func main() {
 
 	// check if folder is empty
 	isEmptyEmail := isFolderEpubEmpty(c)
-	fmt.Println(isEmptyEmail)
+
+	if isEmptyEmail {
+		// get first 10 unread newsletters
+		seqset := getUnreadMessages(c, 10)
+		fmt.Println(seqset)
+
+		//Move top n nesletters to epub
+		log.Println("Moving emails...")
+		if err := c.Move(seqset, os.Getenv("DESTINATION_MAILBOX")); err != nil {
+			log.Fatalf("Error on move to %s: %v", "epub", err)
+		}
+		log.Println("Done")
+	}
 }
